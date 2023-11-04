6607139fe0d2277b00a6c642590925e29dda6307
stabildev <kleehenrik@icloud.com>
2023-11-04 02:34:59 +0100

    m

---
+++ src/components/BillingForm.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { trpc } from '@/app/_trpc/client'
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { useToast } from '@/components/ui/use-toast'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { format } from 'date-fns'
+import { Loader2Icon } from 'lucide-react'
+
+const BillingForm = ({
+  subscriptionPlan,
+}: {
+  subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
+}) => {
+  const { toast } = useToast()
+
+  const { mutate: createStripeSession, isLoading } =
+    trpc.createStripeSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) {
+          window.location.href = url
+        } else {
+          toast({
+            title: 'There was a problem',
+            description: 'Please try again later.',
+            variant: 'destructive',
+          })
+        }
+      },
+    })
+
+  return (
+    <MaxWidthWrapper className="max-w-5xl">
+      <form
+        className="mt-12"
+        onSubmit={(e) => {
+          e.preventDefault()
+          createStripeSession()
+        }}
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Subscription Plan</CardTitle>
+            <CardDescription>
+              You are currently on the <strong>{subscriptionPlan.name}</strong>{' '}
+              plan.
+            </CardDescription>
+          </CardHeader>
+
+          <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+            <Button type="submit">
+              {isLoading ? (
+                <Loader2Icon className="h-4 w-4 animate-spin" />
+              ) : null}
+              {subscriptionPlan.isSubscribed
+                ? 'Manage Subscription'
+                : 'Upgrade to Pro'}
+            </Button>
+
+            {subscriptionPlan.isSubscribed ? (
+              <p className="rounded-full text-xs font-medium">
+                {subscriptionPlan.isCanceled
+                  ? 'Your plan will be canceled on '
+                  : 'Your plan renews on '}
+                {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}
+              </p>
+            ) : null}
+          </CardFooter>
+        </Card>
+      </form>
+    </MaxWidthWrapper>
+  )
+}
+
+export default BillingForm
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -5,6 +5,9 @@ import { db } from '@/db'
 import { z } from 'zod'
 import { $Enums } from '@prisma/client'
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
+import { absoluteUrl } from '@/lib/utils'
+import { getUserSubscriptionPlan, stripe } from '@/lib/stripe'
+import { PLANS } from '@/config/stripe'
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -161,13 +164,67 @@ export const appRouter = router({
         },
       })
 
-      const nextCursor =
-        messages.length > limit ? messages.pop()?.id : null
+      const nextCursor = messages.length > limit ? messages.pop()?.id : null
 
       return {
         messages,
         nextCursor,
       }
     }),
+  createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx
+
+    const billingUrl = absoluteUrl('/dashboard/billing')
+
+    if (!userId) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'Unauthorized',
+      })
+    }
+
+    const dbUser = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+
+    if (!dbUser) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'User not found',
+      })
+    }
+
+    const subscriptionPlan = await getUserSubscriptionPlan()
+
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl,
+      })
+
+      return { url: stripeSession.url }
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ['card', 'paypal'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === 'Pro')?.price.priceIds.test,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    })
+
+    return { url: stripeSession.url }
+  }),
 })
 export type AppRouter = typeof appRouter
