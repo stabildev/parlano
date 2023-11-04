cd5e6312c3f933472a9b9b474d336e61859e17b6
stabildev <kleehenrik@icloud.com>
2023-11-04 11:21:49 +0100

    .

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,8 +9,5 @@ export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') {
     return path
   }
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}${path}`
-  }
   return `${process.env.NEXT_PUBLIC_URL}${path}`
 }
