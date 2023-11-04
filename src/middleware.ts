07f5810536e45d449591231c6a9cd00f29a260b2
stabildev <kleehenrik@icloud.com>
2023-11-04 17:29:10 +0100

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/dist/server'
+import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const config = {
   matcher: ['/dashboard/:path*', '/auth-callback'],
