fbc6bfacb5bb2f968cb3c7fb04507c2456d911e7
stabildev <kleehenrik@icloud.com>
2023-11-10 10:32:43 +0100

    cwtest again

--- src/lib/validators/SendMessageValidator.ts
+++ src/lib/validators/SendMessageValidator.ts
@@ -4,3 +4,5 @@ export const sendMessageValidator = z.object({
   fileId: z.string(),
   message: z.string().min(1).max(1000),
 })
+
+export type SendMessageValidator = z.infer<typeof sendMessageValidator>
