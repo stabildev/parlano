345ceca9ba4fe0fbafef59277a9afe49a0efbf3a
stabildev <kleehenrik@icloud.com>
2023-11-16 14:26:43 +0100

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-  publicRoutes: ['/', '/pricing'],
+  publicRoutes: ['/', '/pricing', '/api/message'],
 })
 
 export const config = {
